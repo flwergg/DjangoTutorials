9473426d37b4f8f1286becd7307f4a0788e9afdf
flwergg <gabymartinez12319@gmail.com>
2025-08-07 20:19:00 -0500

    Add activities 1 to 5

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView
 
 
 urlpatterns = [
     path ('', HomePageView.as_view(), name='home'),
     path ('about/', AboutPageView.as_view(), name='about'),
     path ('products/', ProductIndexView.as_view(), name='index'),
+    path ('contact/', ContactPageView.as_view(), name='contact'),
     path ('products/create', ProductCreateView.as_view(), name='form'),
     path ('products/<str:id>', ProductShowView.as_view(), name='show')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views import View
 from django import forms
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -21,12 +22,15 @@ class AboutPageView(TemplateView):
         })
         return context
     
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+    
 class Product:
     products = [
-        {"id":"1", "name":"TV", "description":"Best TV"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+        {"id":"1", "name":"TV", "description":"Best TV", "price":200},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":800},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":500},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":100}
     ]
 
 class ProductIndexView(View):
@@ -44,11 +48,16 @@ class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
-        viewData = {}
-        product = Product.products[int(id)-1]
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " - Product information"
-        viewData["product"] = product
+        try:
+            product = Product.products[int(id) - 1]
+        except (IndexError, ValueError):
+            return HttpResponseRedirect(reverse('home'))
+        
+        viewData = {
+            "title": product["name"] + " - Online Store",
+            "subtitle": product["name"] + " - Product information",
+            "product": product
+        }
 
         return render(request, self.template_name, viewData)
 
